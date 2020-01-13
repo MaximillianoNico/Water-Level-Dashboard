b3fe93415f339864394dfe87d1849dd416c5ab03
MaximillianoNico <maximillianonico8@gmail.com>
2020-01-13 20:11:51 +0700

    create server api

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const BlynkLib = require('blynk-library');
+
+require('dotenv').config()
+ 
+const blynk = new BlynkLib.Blynk(process.env.BLYNK_API_KEY);
+const v1 = new blynk.VirtualPin(1);
+const v9 = new blynk.VirtualPin(9);
+ 
+v1.on('write', function(param) {
+  console.log('V1:', param);
+});
+ 
+v9.on('read', function() {
+  v9.write(new Date().getSeconds());
+});
